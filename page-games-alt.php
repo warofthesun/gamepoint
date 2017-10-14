e25495cdbb9b296a1fb630b58c1f2093b1299914
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2017-10-13 21:37:09 -0500

    Display with category title as header

---
+++ page-games-alt.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ Template Name: Games Page - Alt
+*/
+?>
+<!--page-games-->
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-3of3 d-7of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php $args = array(
+        						'post_type' => 'games' );
+
+    							$categories = get_categories( $args );
+    							foreach ( $categories as $category ) {
+
+    							echo '<h2>' . $category->name . '</h2>';
+
+        						$args['category'] = $category->term_id;
+        						$posts = get_posts($args); ?>
+
+						        <ul class="menu-items-container">
+						            <?php foreach($posts as $post) { ?>
+						                <li><?php the_title(); ?> <?php echo $item_price; ?></li>
+						            <?php  } ?>
+						        </ul>
+						<?php } ?>
+						</main>
+				</div>
+			</div>
+
+<?php get_footer(); ?>
