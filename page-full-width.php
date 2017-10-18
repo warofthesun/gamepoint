6e9c8d0b1345415aa2d057ba698b86edf7a9a0bc
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2017-10-18 17:36:06 -0500

    Add Full-Width Page template

--- page-full-width.php
+++ page-full-width.php
@@ -16,16 +16,14 @@
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-								<!--header class="article-header">
+								<header class="article-header">
 
 									<h1 class="page-title"><?php the_title(); ?></h1>
 
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
+									
 
 
-								</header-->
+								</header>
 
 								<section class="entry-content cf" itemprop="articleBody">
 									<?php
