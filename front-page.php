11a7d3659ee52c2ae2c142a73b7149de77056c65
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2017-10-18 16:51:52 -0500

    Add most recent blog post to homepage

--- front-page.php
+++ front-page.php
@@ -4,13 +4,56 @@
 				<div id="inner-content" class="wrap cf">
 						<main id="main" class="m-all t-2of3 d-5of7 padding_remove-right cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 							<article>
-								<?php $my_query = new WP_Query('pagename=message-block');
-								while ($my_query->have_posts()) : $my_query->the_post();?>
+								<?php $loop = new WP_Query('pagename=message-block');
+								while ($loop->have_posts()) : $loop->the_post();?>
 
 									<h2><?php the_field('title'); ?></h2>
 									<?php the_content() ?>
 
 								<?php endwhile; ?>
+							</article>
+							<article class="homepage_event">
+
+
+								<?php $loop = new WP_Query( 'posts_per_page=1' ); ?>
+
+								<?php while ($loop -> have_posts()) : $loop -> the_post(); ?>
+									<h2>News</h2>
+									<div class="m-all">
+										<div class="event_title">
+											<h1>
+												<?php the_title('<a href="' . tribe_get_event_link() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a>'); ?>
+											</h1>
+										</div >
+										<div class="event_date">
+																			<?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+														/* the time the post was published */
+														'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+														/* the author of the post */
+														'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+													); ?>
+										</div>
+									</div>
+									<div style="clear:both"></div>
+									<div class="m-all t-all d-all cf">
+										<a href="<?php the_permalink(); ?>">
+											<?php if ( has_post_thumbnail() ) the_post_thumbnail('large'); ?>
+										</a>
+									</div>
+									<div class="m-all t-all d-all cf">
+
+										<div class="event_content">
+											<?php the_excerpt() ?>
+										</div>
+
+									</div>
+
+
+								<?php
+								endwhile;
+								wp_reset_postdata();
+								?>
+								</ul>
 							</article>
 							<article class="homepage_event">
 								<?php $loop = new WP_Query(
