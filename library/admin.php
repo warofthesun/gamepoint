72f79790c82f2b7f6726d68776e817e481c4ccc8
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2017-10-19 19:11:57 -0500

    Update Admin footer

--- library/admin.php
+++ library/admin.php
@@ -138,7 +138,7 @@ you like.
 
 // Custom Backend Footer
 function bones_custom_admin_footer() {
-	_e( '<span id="footer-thankyou">Developed by <a href="http://yoursite.com" target="_blank">Your Site Name</a></span>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.', 'bonestheme' );
+	_e( '<span id="footer-thankyou">Developed by <a href="http://thesplitpixel.com" target="_blank">The SplitPixel</a></span>.', 'bonestheme' );
 }
 
 // adding it to the admin area
