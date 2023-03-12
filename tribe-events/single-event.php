273b431c0d43e7fbf4e87f1bd1ed242e0d3ae875
Dan Brown <fireflip@gmail.com>
2023-03-12 14:20:12 -0500

    Update theme to be compatible with The Events Calendar 6.0

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -27,7 +27,6 @@ $event_id = get_the_ID();
 	<p class="tribe-events-back">
 		<a href="<?php echo esc_url( tribe_get_events_link() ); ?>"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'the-events-calendar' ), $events_label_plural ); ?></a>
 	</p>
-
 	<!-- Notices -->
 	<?php tribe_the_notices() ?>
 
